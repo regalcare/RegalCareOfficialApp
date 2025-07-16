85898d3bc52ad9f1143d4ca8846b4d884bfe528a
regalcare <regalcarehomeservice@gmail.com>
2025-07-15 21:18:52 -0700

    New commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.regalcare.app',
+  appName: 'Regal Care',
+  webDir: 'dist/public'
+};
+
+export default config;
